1f3c87d452e6590dd2ca7f714dc513bce97a8522
shayekh00 <shayekhnavid@gmail.com>
2025-06-21 09:05:49 +0200

    up

--- backend_logic.py
+++ backend_logic.py
@@ -10,6 +10,9 @@ import streamlit as st
 import requests
 import uuid
 
+from moviepy import VideoFileClip 
+import os
+import streamlit as st
 from loomdl import download_loom_video, fetch_loom_download_url, extract_id
     
 def download_video(video_url):
@@ -29,46 +32,6 @@ def download_video(video_url):
 
 
 
-# def audio_extraction(video_path):
-    
-#     audio_path = video_path.replace(".mp4", ".mp3")
-#     try:
-#         st.write(f"Extracting audio from video")
-#         audio_path = "extracted_audio/"+audio_path
-
-#         extract_audio(input_path="./extracted_video/"+video_path, output_path=audio_path)
-
-
-#         return audio_path
-#     except Exception as e:
-#         st.error(f"Audio extraction failed: {e}")
-#         return None
-    
-
-# def audio_extraction(video_path):
-#     audio_path = video_path.replace(".mp4", ".mp3")
-
-#     # # Ensure output directories exist
-#     # os.makedirs("./extracted_audio", exist_ok=True)
-#     # os.makedirs("./extracted_video", exist_ok=True)
-
-#     # final_input_path = os.path.join("extracted_video", video_path)
-#     # final_output_path = os.path.join("extracted_audio", audio_path)
-
-#     final_input_path = "0739fd5b-bab1-4637-909d-43552f36f9ba.mp4"
-#     final_output_path = audio_path
-
-#     try:
-#         st.write(f"Extracting audio from video: {final_input_path}")
-#         ffmpeg.input(final_input_path).output(final_output_path, acodec='mp3', vn=None).run(overwrite_output=True)
-#         return final_output_path
-#     except ffmpeg.Error as e:
-#         raise RuntimeError(f"FFmpeg failed: {e.stderr.decode()}")
-
-
-from moviepy import VideoFileClip 
-import os
-import streamlit as st
 
 def audio_extraction(video_path):
     audio_filename = video_path.replace(".mp4", ".mp3")
